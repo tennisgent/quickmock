c066ecaea3602e2be90a78c9eac66beb8a4fcb0a
Zack <tennisgent@gmail.com>
2015-02-04 21:56:16 -0700

    Beefed up the zb-toggle tests

--- demo/specsUsingQuickMock/zbToggleDirective.spec.js
+++ demo/specsUsingQuickMock/zbToggleDirective.spec.js
@@ -4,7 +4,7 @@
 		var zbToggle;
 
 		beforeEach(function(){
-		    zbToggle = quickmock({
+			zbToggle = quickmock({
 				providerName: 'zbToggle',
 				moduleName: 'QuickMockDemo',
 				mockModules: ['QuickMockDemoMocks'],
@@ -26,17 +26,30 @@
 			// .$compile() accepts a new html string, which will override the
 			// html string previously provided when quickmock was initialized
 			// NOTE: When .$compile() is called, you don't need to call $scope.$digest() again
-		    zbToggle.$compile('<div zb-toggle ng-model="isChecked"><div>Sample Inner Content</div></div>');
+			zbToggle.$compile('<div zb-toggle ng-model="isChecked"><div>Sample Inner Content</div></div>');
 			var span = zbToggle.$element.find('span');
-			expect(span.html()).toContain('Sample Inner Content');
+			expect(span.text()).toEqual('Sample Inner Content');
+		});
+
+		it('should default to unchecked if no model value is previously set', function(){
+			var wasChecked = zbToggle.$element.find('input').attr('checked');
+			expect(wasChecked).toBeFalsy();
+		});
+
+		it('should be checked if model value is truthy', function(){
+			zbToggle.$scope.isChecked = true;
+			zbToggle.$compile('<div zb-toggle ng-model="isChecked"></div>');
+			expect(zbToggle.$isoScope.check).toEqual(true);
+			var wasChecked = zbToggle.$element.find('input').attr('checked');
+			expect(wasChecked).toEqual('checked');
 		});
 
 		it('should toggle the checkbox when clicked', function(){
-			expect(zbToggle.$isoScope.check).toBe(false);
+			expect(zbToggle.$scope.isChecked).toBe(false);
 			zbToggle.$element[0].click();
-			expect(zbToggle.$isoScope.check).toBe(true);
+			expect(zbToggle.$scope.isChecked).toBe(true);
 			zbToggle.$element[0].click();
-			expect(zbToggle.$isoScope.check).toBe(false);
+			expect(zbToggle.$scope.isChecked).toBe(false);
 		});
 
 		it('should show a success message when toggled to true', function(){
