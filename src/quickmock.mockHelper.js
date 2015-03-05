acfb06d83ba5efb410181b382238403c0257ec32
Zack <tennisgent@gmail.com>
2015-03-04 19:43:38 -0700

    Converted mockHelper over to angular services

--- src/quickmock.mockHelper.js
+++ src/quickmock.mockHelper.js
@@ -1,71 +1,80 @@
 (function(){
 
-	if(!window.quickmock){
-		throw new Error('quickmock.mockHelper: Cannot be initialized because quickmock is not available');
-	}
+	var origModuleFunc;
 
-	var hasBeenMocked = {},
-		origModuleFunc = angular.module;
+    angular.module('quickmock.mockHelper', ['quickmock'])
 
-	angular.module = decorateAngularModule;
+        .run(['decorateAngularModule',
+            function(decorateAngularModule){
+                origModuleFunc = angular.module;
+                angular.module = decorateAngularModule;
+            }
+        ])
 
-	quickmock.mockHelper = {
-		hasBeenMocked: hasBeenMocked
-	};
+        //.factory('origModuleFunc', )
 
-	function decorateAngularModuleObject(modObj){
-		var methods = getDecoratedMethods(modObj);
-		angular.forEach(methods, function(method, methodName){
-			modObj[methodName] = method;
-		});
-		return modObj;
-	}
+        .service('decorateAngularModule', ['getDecoratedMethods',
+            function(getDecoratedMethods){
+                return function decorateAngularModule(name, requires, configFn){
+                    var modObj = origModuleFunc(name, requires, configFn),
+                        methods = getDecoratedMethods(modObj);
+                    angular.forEach(methods, function(method, methodName){
+                        modObj[methodName] = method;
+                    });
+                    return modObj;
+                };
+            }
+        ])
 
-	function decorateAngularModule(name, requires, configFn){
-		var modObj = origModuleFunc(name, requires, configFn);
-		return decorateAngularModuleObject(modObj);
-	}
+        .service('getDecoratedMethods', ['global',
+            function(global){
+                return function getDecoratedMethods(modObj){
 
-	function getDecoratedMethods(modObj){
+                    function basicMock(providerName, initFunc, providerType){
+                        var newModObj = modObj[providerType](global.mockPrefix() + providerName, initFunc),
+                            methods = getDecoratedMethods(newModObj);
+                        angular.forEach(methods, function(method, methodName){
+                            newModObj[methodName] = method;
+                        });
+                        return newModObj;
+                    }
 
-		function basicMock(providerName, initFunc, providerType){
-			hasBeenMocked[providerName] = true;
-			var newModObj = modObj[providerType](quickmock.getMockPrefix() + providerName, initFunc);
-			return decorateAngularModuleObject(newModObj);
-		}
+                    return {
+                        mockService: function mockService(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'service', modObj);
+                        },
+                        mockFactory: function mockFactory(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'factory', modObj);
+                        },
 
-		return {
-			mockService: function mockService(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'service', modObj);
-			},
-			mockFactory: function mockFactory(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'factory', modObj);
-			},
+                        mockFilter: function mockFilter(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'filter', modObj);
+                        },
 
-			mockFilter: function mockFilter(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'filter', modObj);
-			},
+                        mockController: function mockController(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'controller', modObj);
+                        },
 
-			mockController: function mockController(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'controller', modObj);
-			},
+                        mockProvider: function mockProvider(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'provider', modObj);
+                        },
 
-			mockProvider: function mockProvider(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'provider', modObj);
-			},
+                        mockValue: function mockValue(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'value', modObj);
+                        },
 
-			mockValue: function mockValue(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'value', modObj);
-			},
+                        mockConstant: function mockConstant(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'constant', modObj);
+                        },
 
-			mockConstant: function mockConstant(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'constant', modObj);
-			},
+                        mockAnimation: function mockAnimation(providerName, initFunc){
+                            return basicMock(providerName, initFunc, 'animation', modObj);
+                        }
+                    };
+                };
+            }
+        ]);
 
-			mockAnimation: function mockAnimation(providerName, initFunc){
-				return basicMock(providerName, initFunc, 'animation', modObj);
-			}
-		};
-	}
+    angular.bootstrap(angular.element('<div></div>'), ['quickmock.mockHelper']);
 
 })();
